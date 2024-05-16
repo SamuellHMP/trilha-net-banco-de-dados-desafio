ff6bbd5223510e7f42fce5bed2cb6e9f273ece17
Samuell Henrique de Magalhães Perpétuo <samuelhmp@hotmail.com>
2024-05-16 11:25:58 -0300

    Update Script Solucao.sql

--- Script Solucao.sql
+++ Script Solucao.sql
@@ -1,10 +1,10 @@
--- 1 - Buscar o nome e ano dos filmes
+-- 1 - Nome e ano dos filmes
 SELECT
 	Nome,
 	Ano
 FROM Filmes
 
--- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+-- 2 - Nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT
 	Nome,
 	Ano,
@@ -12,7 +12,7 @@ SELECT
 FROM Filmes
 ORDER BY Ano
 
--- 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a dura��o
+-- 3 - Filme 'De Volta para o Futuro': nome, ano e a duracao
 SELECT
 	Nome,
 	Ano,
@@ -20,7 +20,7 @@ SELECT
 FROM Filmes
 WHERE Nome = 'De Volta para o Futuro'
 
--- 4 - Buscar os filmes lan�ados em 1997
+-- 4 - Filmes lancados em 1997
 SELECT
 	Nome,
 	Ano,
@@ -28,7 +28,7 @@ SELECT
 FROM Filmes
 WHERE Ano = 1997
 
--- 5 - Buscar os filmes lan�ados AP�S o ano 2000
+-- 5 - Filmes lancados apos o ano 2000
 SELECT
 	Nome,
 	Ano,
@@ -36,8 +36,8 @@ SELECT
 FROM Filmes
 WHERE Ano > 2000
 
--- 6 - Buscar os filmes com a duracao maior que 100 e menor que 150,
--- ordenando pela duracao em ordem crescente
+-- 6 - Filmes com duracao maior que 100 e menor que 150,
+-- ordenado pela duracao em ordem crescente
 SELECT
 	Nome,
 	Ano,
@@ -46,8 +46,8 @@ FROM Filmes
 WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
 
--- 7 - Buscar a quantidade de filmes lan�adas no ano, agrupando por ano,
--- ordenando pela duracao em ordem decrescente
+-- 7 - Quantidade de filmes lancadas no ano, agrupando por ano,
+-- ordenado pela duracao em ordem decrescente
 SELECT
 	Ano,
 	COUNT(1) Quantidade
@@ -55,21 +55,21 @@ FROM Filmes
 GROUP BY Ano
 ORDER BY Quantidade DESC
 
--- 8 - Buscar os Atores do g�nero masculino, retornando o PrimeiroNome, UltimoNome
+-- 8 - Atores do genero masculino: PrimeiroNome, UltimoNome
 SELECT
 	*
 FROM Atores
 WHERE Genero = 'M'
 
--- 9 - Buscar os Atores do g�nero feminino, retornando o PrimeiroNome, UltimoNome,
--- e ordenando pelo PrimeiroNome
+-- 9 - Atores do genero feminino: PrimeiroNome, UltimoNome,
+-- ordenado pelo PrimeiroNome
 SELECT
 	*
 FROM Atores
 WHERE Genero = 'F'
 ORDER BY PrimeiroNome
 
--- 10 - Buscar o nome do filme e o g�nero
+-- 10 - Nomes dos filmes e o genero
 SELECT
 	f.Nome,
 	g.Genero
@@ -77,16 +77,16 @@ FROM Filmes f
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON fg.IdGenero = g.Id
 
--- 11 - Buscar o nome do filme e o g�nero do tipo "Mist�rio"
+-- 11 - Nome do filme e o genero do tipo "Mistério"
 SELECT
 	f.Nome,
 	g.Genero
 FROM Filmes f
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON fg.IdGenero = g.Id
-WHERE g.Genero = 'Mist�rio'
+WHERE g.Genero = 'Mistério'
 
--- 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+-- 12 - Nome do filme e os atores: PrimeiroNome, UltimoNome e Papel
 SELECT
 	f.Nome,
 	a.PrimeiroNome,
